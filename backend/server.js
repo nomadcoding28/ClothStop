7dd33cb9780166a45cdfabcc28c83e6e98003938
nomadcoding28 <fairlyanonymous007@gmail.com>
2024-11-20 01:49:01 +0530

    Udpated server to read credentials

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 dotenv.config();
 const app= express();
 const PORT=process.env.PORT || 5000;
 
 app.use("/api/auth",authRoutes);
 
-
+app.use(express.json());// it is used to parse the json data from the request body
 
 app.listen(PORT,() =>{
     console.log("Server is running on http://localhost:"+PORT);
+    connectDB();
 })
