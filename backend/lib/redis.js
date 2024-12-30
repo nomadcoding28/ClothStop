ec27ea0e17f2254346b45e27f2d47eeeaf9ceb21
nomadcoding28 <fairlyanonymous007@gmail.com>
2024-12-30 15:16:20 +0530

    checking if it is connected to redis

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -2,4 +2,7 @@ import Redis from "ioredis"
 import dotenv from "dotenv";
 dotenv.config();
 export const client = new Redis(process.env.UPSTACH_URL);
+client.on('connect', () => console.log('Connected to Redis'));
+client.on('error', (err) => console.error('Redis connection error:', err));
+
 // await client.set('foo', 'bar'); 
