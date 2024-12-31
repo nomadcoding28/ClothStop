6bc06d1d4bd95cc91f7fd921e3b120d02773236b
nomadcoding28 <fairlyanonymous007@gmail.com>
2024-12-31 23:12:27 +0530

    updated by addinf product route

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,7 @@
-// import express from "express";
-// import dotenv from "dotenv";
-// import authRoutes from "./routes/auth.route.js";
-// import { connectDB } from "./lib/db.js";
-// dotenv.config();
-// const app= express();
-// const PORT=process.env.PORT || 5000;
-
-
-
-// app.use(express.json());// it is used to parse the json data from the request body
-// app.use(express.urlencoded({ extended: true }));// it is used to parse the url encoded data from the request body
-// app.use("/api/auth",authRoutes);
-// // app.use((err, req, res, next) => {
-// //     console.error(err.stack);
-// //     res.status(500).send('Something broke!');
-// // });
-
-// app.get("/",(req,res)=>{
-//     res.send("Home route called");
-// })
-// app.post("/signup",(req,res)=>{
-//     res.send("Sign up route called");});
-
-// app.listen(PORT,() =>{
-//     console.log("Server is running on http://localhost:"+PORT);
-//     connectDB();
-// })
-
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js"
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js"; // Ensure this function establishes a proper DB connection
 
@@ -44,6 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Mount authentication routes
 app.use("/api/auth", authRoutes);
+app.use("/api/products",productRoutes);
 
 // Debugging Route for Testing GET Requests
 app.get("/api/auth/signup", (req, res) => {
