fc41234390ce0a104ba957f90d7e4a15081e2195
nomadcoding28 <fairlyanonymous007@gmail.com>
2025-01-02 16:19:06 +0530

    added togglefeatureproduct function and updated the route

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,3 +1,4 @@
+import { client } from "../lib/redis.js";
 import Product from "../models/product.model.js";
 export const addProducts=async(req,res)=>{
     try{
@@ -12,11 +13,30 @@ export const addProducts=async(req,res)=>{
 
 export const getFeaturedProducts=async(req,res)=>{
     try{
-        const products=await Product.find({isFeatured:true});
-        if(!products){
-            res.status(404).json({message:"No featured products found"});
-        }
-        res.json({message:"Featured products fetched successfully",products});
+        // const products=await Product.find({isFeatured:true});
+        // if(!products){
+        //     res.status(404).json({message:"No featured products found"});
+        // }
+        // res.json({message:"Featured products fetched successfully",products});
+        let featuredProducts = await  client.get("featured_products");
+		if (featuredProducts) {
+			return res.json(JSON.parse(featuredProducts));
+		}
+
+		// if not in redis, fetch from mongodb
+		// .lean() is gonna return a plain javascript object instead of a mongodb document
+		// which is good for performance
+		featuredProducts = await Product.find({ isFeatured: true }).lean();
+
+		if (!featuredProducts) {
+			return res.status(404).json({ message: "No featured products found" });
+		}
+
+		// store in redis for future quick access
+
+		await client.set("featured_products", JSON.stringify(featuredProducts));
+
+		res.json(featuredProducts);
 
     }catch(error){
         res.status(500).send("Error fetching featured products");
@@ -36,6 +56,28 @@ export const deleteProduct=async(req,res)=>{
         res.json({message:"Product deleted successfully"});
     } catch (error) {
         res.status(500).send("error in deleting product");
+    }
+}
+export const getRecommendedProducts=async(req,res)=>{
+    try {
+        const products=await Product.aggregate([
+            {
+                $sample:{size:2}
+
+            },
+            {
+                $project:{
+                    name:1,
+                    image:1,
+                    price:1,
+                    _id:0,
+                    description:1,
+                }
+            }
+        ])
+        res.json({message:"Recommended products fetched successfully",products});
+    } catch (error) {
+        res.status(500).send("Error fetching recommended products");
     }
 }
 export const getProductByCategory=async(req,res)=>{
@@ -60,4 +102,28 @@ export const getAllProducts =async(req,res)=>{
         res.status(500).send("Error fetching products");
     }
     
+}
+export const toggleFeaturedProduct=async (req,res) => {
+    try {
+        const product =await Product.findById(req.params.id);
+        if(!product){
+            return res.status(404).json({message:"no product found"});
+        }
+        product.isFeatured=!product.isFeatured;
+        const updatedProduct = await product.save();
+        await updateFeaturedProductsCache();
+        res.json({message:"Product updated successfully",updatedProduct});
+    } catch (error) {
+        res.status(500).send("Error updating product");
+    }
+}
+async function updateFeaturedProductsCache() {
+	try {
+		// The lean() method  is used to return plain JavaScript objects instead of full Mongoose documents. This can significantly improve performance
+
+		const featuredProducts = await Product.find({ isFeatured:true }).lean();
+		await client.set("featured_products", JSON.stringify(featuredProducts));
+	} catch (error) {
+		console.log("error in update cache function");
+	}
 }
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllProducts ,addProducts, getFeaturedProducts, deleteProduct, getProductByCategory} from "../controllers/product.controller.js";
+import { getAllProducts ,addProducts, getFeaturedProducts, deleteProduct, getProductByCategory, getRecommendedProducts, toggleFeaturedProduct} from "../controllers/product.controller.js";
 // import{protectRoute,adminRoute} from "../middleware/auth.middleware.js"
 const router = express.Router();
 router.get("/",getAllProducts);
@@ -7,4 +7,6 @@ router.post("/",addProducts);
 router.get("/featured",getFeaturedProducts)
 router.delete("/:id",deleteProduct)
 router.get("/category/:category",getProductByCategory)
+router.get("/recommended",getRecommendedProducts)
+router.patch("/:id",toggleFeaturedProduct)
 export default router
